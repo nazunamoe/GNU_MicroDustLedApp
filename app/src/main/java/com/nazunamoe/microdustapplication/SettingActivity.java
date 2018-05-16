192c322245adfe9de586dba79852a5925f215d34
nazunamoe <jpg3927@gmail.com>
2018-05-17 06:40:40 +0900

    ddd

--- app/src/main/java/com/nazunamoe/microdustapplication/SettingActivity.java
+++ app/src/main/java/com/nazunamoe/microdustapplication/SettingActivity.java
@@ -1,20 +1,84 @@
 package com.nazunamoe.microdustapplication;
 
+import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.nazunamoe.microdustapplication.R;
 
-public class SettingActivity extends AppCompatActivity {
-
+public class SettingActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
+    SharedPreferences sharedPref;
+    SettingsFragment settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_setting);
+        PreferenceManager.setDefaultValues(this, R.xml.settings_preferences, false);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        settings = new SettingsFragment();
+        FragmentTransaction trans = getFragmentManager().beginTransaction();
+        trans.replace(android.R.id.content, settings);
+        trans.commit();
+    }
 
+    private void pickPreferenceObject(Preference p) {
+        if (p instanceof PreferenceCategory) {
+            PreferenceCategory cat = (PreferenceCategory) p;
+            for (int i = 0; i < cat.getPreferenceCount(); i++) {
+                pickPreferenceObject(cat.getPreference(i));
+            }
+        } else {
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Log.d("fuck","WHATTHE"+key);
+    }
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_preferences);
+            for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+                pickPreferenceObject(getPreferenceScreen().getPreference(i));
+            }
+        }
+        private void pickPreferenceObject(Preference p) {
+            if (p instanceof PreferenceCategory) {
+                PreferenceCategory cat = (PreferenceCategory) p;
+                for (int i = 0; i < cat.getPreferenceCount(); i++) {
+                    pickPreferenceObject(cat.getPreference(i));
+                }
+            } else {
+            }
+        }
 
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sharedPref.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sharedPref.unregisterOnSharedPreferenceChangeListener(this);
+    }
 }
+
